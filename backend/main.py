9cb7a2e43ce2de9f8eda4a07065cac76ce9f3e64
Jaswanth <128232804+Jaswanth5996@users.noreply.github.com>
2025-04-13 08:27:52 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -296,6 +296,6 @@ def health_check():
 CORS(app, origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://ai-router-three.vercel.app/"])
 
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Use Render's port or fallback to 5000
     app.run(host='0.0.0.0', port=port)
